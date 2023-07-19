c8b77a08c47fb3c1c536c867e93552791ab622aa
TypingOK <wjdwls717@gmail.com>
2023-07-19 11:52:47 +0900

    Update:: 허스키 초기 셋팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { LOGOUT } from "@/constants/a";
 
 export default function Home() {
-  return <main className="text-primary-100 Body1_20_B">{LOGOUT}</main>;
+  return (
+    <main className="text-primary-100 Body1_20_B">{LOGOUT}커밋테스트</main>
+  );
 }
