ea50c2dc418261b6c3a1c71e1018774559b13fdd
TypingOK <wjdwls717@gmail.com>
2023-07-19 11:25:21 +0900

    husky test setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { LOGOUT } from "@/constants/a";
 
 export default function Home() {
-  return <main className="text-primary-100 Body1_20_B">{LOGOUT}test</main>;
+  return <main className="text-primary-100 Body1_20_B">{LOGOUT}</main>;
 }
