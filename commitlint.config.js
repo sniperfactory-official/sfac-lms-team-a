ca9c929802bbbb38346768e16fbd1602e599b67b
TypingOK <wjdwls717@gmail.com>
2023-07-19 13:35:57 +0900

    Test: 허스키 테스트 커밋

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  // rules: {
-  //   "type-enum": [
-  //     2,
-  //     "always",
-  //     [
-  //       "Feat:: ",
-  //       "Update:: ",
-  //       "FIX:: ",
-  //       "!BREAKING CHANGE:: ",
-  //       "!HOTFIX:: ",
-  //       "Style:: ",
-  //       "Refactor:: ",
-  //       "Comment:: ",
-  //       "Chore:: ",
-  //       "Docs:: ",
-  //       "Test:: ",
-  //       "Rename:: ",
-  //       "Remove:: ",
-  //     ],
-  //   ],
-  // },
+  rules: {
+    "type-case": [2, "always", "pascal-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Feat",
+        "Update",
+        "FIX",
+        "!BREAKING CHANGE",
+        "!HOTFIX",
+        "Style",
+        "Refactor",
+        "Comment",
+        "Chore",
+        "Docs",
+        "Test",
+        "Rename",
+        "Remove",
+      ],
+    ],
+  },
 };
