462086d8dca5a0a32cf88349c16eb9697de486bd
TypingOK <wjdwls717@gmail.com>
2023-07-19 11:53:03 +0900

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { LOGOUT } from "@/constants/a";
 
 export default function Home() {
   return (
-    <main className="text-primary-100 Body1_20_B">{LOGOUT}커밋테스트</main>
+    <main className="text-primary-100 Body1_20_B">{LOGOUT}커밋테스트111</main>
   );
 }
