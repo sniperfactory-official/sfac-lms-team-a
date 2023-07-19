21b486235d43997fde20453bfe562025aaa6721c
TypingOK <wjdwls717@gmail.com>
2023-07-19 12:03:05 +0900

    123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Feat:: ",
+        "Update:: ",
+        "FIX:: ",
+        "!BREAKING CHANGE:: ",
+        "!HOTFIX:: ",
+        "Style:: ",
+        "Refactor:: ",
+        "Comment:: ",
+        "Chore:: ",
+        "Docs:: ",
+        "Test:: ",
+        "Rename:: ",
+        "Remove:: ",
+      ],
+    ],
+  },
 };
