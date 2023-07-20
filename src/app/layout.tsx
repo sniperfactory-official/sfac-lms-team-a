37beb76ff8b4268c0b34753bb8b761191939768f
lgrin-byte <lgrin02@naver.com>
2023-07-20 18:45:04 +0900

    Feat: Footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Provider from "./Provider";
-
+import Footer from "@/components/Footer/Footer"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider>{children}</Provider>
+        <Footer/>
+
       </body>
     </html>
   );
