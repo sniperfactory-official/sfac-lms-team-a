8f351672d0b2935c96a5062b74341734b42c9479
energy1-914 <dlawldnjs5@daum.net>
2023-07-22 19:48:23 +0900

    Feat: firebase 로그인 function 추가

---
+++ src/utils/firebase.ts
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  setPersistence,
+  signInWithEmailAndPassword,
+  browserSessionPersistence,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCc3R0n2ALsZIVcQdRooXMjjTYl7m-abRg",
+  authDomain: "sniperfactory-lms.firebaseapp.com",
+  projectId: "sniperfactory-lms",
+  storageBucket: "sniperfactory-lms.appspot.com",
+  messagingSenderId: "780057773316",
+  appId: "1:780057773316:web:b9c45f9434044672247c51",
+  measurementId: "G-3DW6BWMNXB",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+
+export const login = (email: string, password: string) =>
+  setPersistence(auth, browserSessionPersistence).then(() => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        return user.uid;
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        if (errorCode === "auth/user-not-found") {
+          alert("등록되지 않은 아이디입니다.");
+        } else if (errorCode === "auth/wrong-password") {
+          alert("비밀번호가 일치하지 않습니다.");
+        } else {
+          alert(errorCode);
+        }
+      });
+  });
