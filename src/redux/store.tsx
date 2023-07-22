e66e1512226fa7795e433dfb62a64bf5b6e7da4b
energy1-914 <dlawldnjs5@daum.net>
2023-07-22 20:05:14 +0900

    Feat: userSlice 생성

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    userId: userSlice,
+  },
   devTools: process.env.NODE_ENV !== "production",
 });
 
