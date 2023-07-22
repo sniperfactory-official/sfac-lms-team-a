3566c5f00d1f493e984381ebbd5962a3842668d4
mibu119 <horang296@gmail.com>
2023-07-23 00:18:12 +0900

    Comment: firebase 사용안하는 코드 주석 해제

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
