afb62ef18de90927143517ba1eab36e3f252ce89
TypingOK <wjdwls717@gmail.com>
2023-07-24 17:39:17 +0900

    Feat: team4 기본 파일 셋팅

---
+++ src/app/classroom/layout.tsx
@@ -0,0 +1,9 @@
+const ClassroomLayout = ({ children }:{ children:React.ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default ClassroomLayout;
---
+++ src/app/classroom/page.tsx
@@ -0,0 +1,7 @@
+const ClassroomPage = () => {
+    return (
+        <div>강의실 페이지 입니다.</div>
+    )
+}
+
+export default ClassroomPage;
