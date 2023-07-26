b0292fb3eea06b889f4ee7dd1b516070be50316e
TypingOK <wjdwls717@gmail.com>
2023-07-26 20:11:02 +0900

    Feat: 강의실 커뮤니티 구현

--- src/app/lectureHall/page.tsx
+++ src/app/lectureHall/page.tsx
@@ -1,11 +1,11 @@
 import ContentArea from "./(components)/Wrapper";
 
 const LectureHall = () => {
-    return (
-        <div className="w-full h-full">
-            <ContentArea id={"mVwanklxft7kGVxiCpaq"}/>
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-full min-h-screen">
+      <ContentArea id={"mVwanklxft7kGVxiCpaq"} />
+    </div>
+  );
+};
 
 export default LectureHall;
