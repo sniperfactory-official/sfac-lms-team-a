a202938060b3fadc280684aad412f508479f5a92
energy1-914 <dlawldnjs5@daum.net>
2023-07-26 11:19:17 +0900

    Refactor: tab바 위치 변경

--- src/components/RequireAuth/RequireAuth.jsx
+++ src/components/RequireAuth/RequireAuth.jsx
@@ -1,24 +1,17 @@
-import React, { useContext } from "react";
 import Navbar from "@/components/Header/Navbar";
 import Tab from "@/components/Header/Tab";
 import Footer from "@/components/Footer/Footer";
-import { useSelector } from "react-redux";
-import { update } from "@/redux/userSlice";
+import { useAppSelector } from "@/redux/store";
 
 export default function RequireAuth({ children }) {
-  const uid = useSelector(state => state.userId.uid);
-
-  console.log(uid);
-  // const uid =true;
-  // const uid = useSelector((state) => state.userSlice.useSelector);
-  // console.log(uid);
+  const uid = useAppSelector(state => state.userId.uid);
 
   return (
     <>
       {uid ? (
         <>
-          <Tab />
           <Navbar />
+          <Tab />
           {children}
           <Footer />
         </>
