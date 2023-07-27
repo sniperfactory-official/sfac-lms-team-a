1b6edac08d829bddb0519023751f16dab790a6b2
TypingOK <wjdwls717@gmail.com>
2023-07-27 15:39:18 +0900

    Test: 머지 테스트

--- src/app/lectureHall/(components)/(LetcureArea)/Footer.tsx
+++ src/app/lectureHall/(components)/(LetcureArea)/Footer.tsx
@@ -1,10 +1,14 @@
 const LectureFooter = () => {
-    return (
-        <div className="w-full h-full flex justify-between">
-            <button className="ml-10 text-lg font-bold text-grayscale-60">이전강의</button>
-            <button className="mr-10 text-lg font-bold text-grayscale-60">다음강의</button>
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-full flex justify-between">
+      <button className="ml-10 text-lg font-bold text-grayscale-60">
+        이전강의
+      </button>
+      <button className="mr-10 text-lg font-bold text-grayscale-60">
+        다음강의
+      </button>
+    </div>
+  );
+};
 
 export default LectureFooter;
--- src/app/lectureHall/(components)/(communityArea)/Community.tsx
+++ src/app/lectureHall/(components)/(communityArea)/Community.tsx
@@ -4,13 +4,11 @@ import useGetLectureCommentQuery from "@/hooks/reactQuery/lecture/useGetLectureC
 import { getTime } from "@/utils/getTime";
 
 const LectureCommunityWrapper = () => {
-  const { data,isLoading } = useGetLectureCommentQuery("mVwanklxft7kGVxiCpaq");
+  const { data, isLoading } = useGetLectureCommentQuery("mVwanklxft7kGVxiCpaq");
   if (isLoading) {
-    return (
-      <div>불러오는 중...</div>
-    )
+    return <div>불러오는 중...</div>;
   }
-  if (data !== undefined ) {
+  if (data !== undefined) {
     return (
       <div className="bg-grayscale-10 w-full h-full p-4 overflow-y-auto">
         <div className="flex justify-between mb-3">
@@ -18,23 +16,29 @@ const LectureCommunityWrapper = () => {
           <button>작성</button>
         </div>
         <div>
-          {
-            data.map((e, i) => (
-              <button key={i} className="w-full min-h-[90px] bg-white rounded-2xl p-4 flex items-center justify-center">
-                <div className="w-11">
-                  {e.user.profile }
+          {data.map((e, i) => (
+            <button
+              key={i}
+              className="w-full min-h-[90px] bg-white rounded-2xl p-4 flex items-center justify-center"
+            >
+              <div className="w-11">{e.user.profile}</div>
+              <div className="flex-1">
+                <div className="flex">
+                  <div className="mr-1 text-base font-bold">
+                    {e.user.username}
+                  </div>{" "}
+                  · <div className="text-grayscale-40 ml-1">{e.user.role}</div>
                 </div>
-                <div className="flex-1">
-                  <div className="flex"><div className="mr-1 text-base font-bold">{e.user.username}</div> · <div className="text-grayscale-40 ml-1">{ e.user.role }</div></div>
-                  <div className="text-sm w-full flex">{e.content}</div>
-                </div>
-                <div className="text-grayscale-40 text-xs ">{ getTime(e.createdAt.toDate()) }</div>
-              </button>
-            ))
-          }
+                <div className="text-sm w-full flex">{e.content}</div>
+              </div>
+              <div className="text-grayscale-40 text-xs ">
+                {getTime(e.createdAt.toDate())}
+              </div>
+            </button>
+          ))}
         </div>
       </div>
-    )
+    );
   }
 };
 
