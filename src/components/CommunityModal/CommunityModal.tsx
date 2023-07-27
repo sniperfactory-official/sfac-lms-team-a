e4b490d0e54cba5442f352929cc1ff880a22ba39
lgrin-byte <lgrin02@naver.com>
2023-07-27 21:42:14 +0900

    Feat: 커뮤니티 모달 레이아웃, 데이터 연결

--- src/components/CommunityModal/CommunityModal.tsx
+++ src/components/CommunityModal/CommunityModal.tsx
@@ -6,14 +6,25 @@ import { useAppSelector } from "@/redux/store";
 import LoadingSpinner from "@/components/Loading/Loading";
 import Image from "next/image";
 import avatar from "/public/images/avatar.svg";
+import PostButton from "@/components/common/PostButton";
+import { useForm } from "react-hook-form";
+
+interface FormValue {
+  content: string;
+}
 
 export default function CommunityModal() {
   const postId = "YiJVx6OQBhlGGRCUj1WU";
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm<FormValue>();
+
   const { data, isLoading, isError, error } = fetchPost(postId);
   console.log(data);
-  const imgArr = [1, 2, 3];
-  const tagArr = [1, 2, 3];
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -25,7 +36,7 @@ export default function CommunityModal() {
 
   return (
     <ModalWrapper modalTitle="상세보기">
-      <div className="border-solid border-2  border-gray-200 rounded-xl p-4 my-6 text-sm">
+      <div className="border-solid border border-gray-200 rounded-xl p-4 my-6 text-sm">
         <div className="flex items-center">
           <Image src={avatar} alt="" className="w-10 h-10 mr-2" />
 
@@ -35,38 +46,43 @@ export default function CommunityModal() {
           <div className="bg-gray-600 w-1 h-1 rounded mx-2"></div>
           <span className="text-gray-600">2023/06/28</span>
         </div>
-        <h2 className="text-base font-bold my-2 ">
-          Lorem Ipsum is simply dummy text of the printing
-        </h2>
+        <h2 className="text-base font-bold my-2 ">{data.title}</h2>
         <div>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining...
-          <div className="grid">
-            {imgArr.map((img, idx) => (
-              <Image />
+          <div className="mb-3">{data.content}</div>
+          <div className="flex">
+            {data.images?.map((img, idx) => (
+              <>
+                <Image
+                  src={img}
+                  alt="post 이미지"
+                  className=" w-14 h-14 rounded-md mr-2 my-6"
+                />
+              </>
             ))}
           </div>
-          <div className="inline-grid grid-cols-3 gap-3">
-            {tagArr.map((tag, idx) => (
-              <span className="bg-gray-100 text-xs py-1 px-2 rounded">
-                태그
+          <div className="">
+            {data.tags?.map((tag, idx) => (
+              <span
+                key={idx}
+                className="bg-gray-100 text-xs py-1 px-2 mr-2 rounded"
+              >
+                {tag}
               </span>
             ))}
           </div>
         </div>
       </div>
 
-      <div className="flex items-center text-lg">
+      <div className="flex items-center text-base border-solid border  border-gray-200 rounded-xl p-4 my-6 ">
         <Image src={avatar} alt="" className="w-10 h-10 mr-2" />
-        <div>
-          <div className="flex items-center">
-            <span>김관우</span>
-            <div className="bg-gray-400 w-1 h-1 rounded mx-2"></div>
-            <span className="text-gray-400">수강생</span>
+        <div className=" w-full">
+          <div className="flex items-center ">
+            <div className="flex items-center flex-1">
+              <span>김관우</span>
+              <div className="bg-gray-400 w-1 h-1 rounded mx-2"></div>
+              <span className="text-gray-400">수강생</span>
+            </div>
+
             <div className="flex divide-x-2 divide-gray text-sm">
               <div>
                 <button className="mr-1">수정</button>
@@ -76,10 +92,32 @@ export default function CommunityModal() {
               </div>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex w-full justify-between mt-1">
             <p className="text-base">이해가 잘 됩니다!</p>
             <span className="text-gray-400 text-sm">3일 전</span>
           </div>
+        </div>
+      </div>
+
+      <div className="flex items-center text-base border-solid border  border-gray-200 rounded-xl p-4 ">
+        <div className=" w-full">
+          <div className="flex items-center ">
+            <Image src={avatar} alt="" className="w-10 h-10 mr-2" />
+            <div className="flex items-center flex-1">
+              <span>김관우</span>
+              <div className="bg-gray-400 w-1 h-1 rounded mx-2"></div>
+              <span className="text-gray-400">수강생</span>
+            </div>
+          </div>
+          <form
+            className="flex w-full mt-1"
+            onSubmit={handleSubmit(data => {
+              data;
+            })}
+          >
+            <input className="text-base flex-1 mr-4 px-1" />
+            <PostButton text="업로드"></PostButton>
+          </form>
         </div>
       </div>
     </ModalWrapper>
