41d427703ea0ebe5d380906f3fbbd8e794a5a79b
Majesty-jun <hkwns01@gmail.com>
2023-07-31 16:12:33 +0900

    Feat: CreateLinkLecture 링크 강의 생성 컴포넌트 생성

---
+++ src/app/classroom/(components)/CreateLinkLecture.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Lecture, LectureContent } from "@/types/firebase.types";
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+interface Props {
+  setLecture: React.Dispatch<React.SetStateAction<Lecture>>;
+}
+
+export default function CreateLinkLecture({ setLecture }: Props) {
+  const [lectureContent, setLectureContent] = useState<LectureContent>({
+    images: [],
+    textContent: "",
+    externalLink: "",
+    videoUrl: "",
+    videoLength: 0,
+  });
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setLectureContent(prev => {
+      return { ...prev, externalLink: e.target.value };
+    });
+  };
+
+  useEffect(() => {
+    setLecture(prev => {
+      return { ...prev, lectureContent: lectureContent };
+    });
+  }, [setLecture, lectureContent]);
+
+  return (
+    <div>
+      <label htmlFor="link" className="sr-only">
+        강의 링크 입력
+      </label>
+      <input
+        type="text"
+        id="link"
+        placeholder="https://..."
+        className="w-[700px] h-[42px] border border-grayscale-10 text-[16px] pl-[12px] py-[8px] rounded-xl"
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+}
