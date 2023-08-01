e55b5aedc76306a9238e50fa50d70cd8faf002d0
hobin49 <hobin505@gmail.com>
2023-08-02 08:42:48 +0900

    !HOTFIX: 로그인 안 되면 다른 페이지로 이동 제한

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,27 +1,65 @@
-import Navbar from "@/components/Header/Navbar";
+// import Navbar from "@/components/Header/Navbar";
 import Tab from "@/components/Header/Tab";
 import Footer from "@/components/Footer/Footer";
-import { useAppSelector } from "@/redux/store";
+import { auth } from "@/utils/firebase";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import LoadingSpinner from "../Loading/Loading";
 
 export default function RequireAuth({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const uid = useAppSelector(state => state.userId.uid);
 
-  return (
-    <>
-      {uid ? (
+  const router = useRouter();
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    }
+  }, []);
+
+  useEffect(() => {
+    // 로그인이 되지 않았거나
+    if (!loading && !authenticated) {
+      router.push("/");
+    } else {
+      router.push("/community")
+    }
+  }, [loading, authenticated]);
+
+  if (loading) {
+    <LoadingSpinner />
+  } else {
+    if (authenticated) {
+      return (
         <>
-          <Navbar />
+          {/* <Navbar /> */}
           <Tab />
           {children}
           <Footer />
         </>
-      ) : (
-        <>{children}</>
-      )}
-    </>
-  );
+      )
+    } else {
+      return (
+        <>
+          {children}
+        </>
+      )
+    }
+  }
+
 }
