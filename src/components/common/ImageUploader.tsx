c17abf35e19c0b4a74d5d13c3bc8f164db27c932
energy1-914 <dlawldnjs5@daum.net>
2023-08-02 02:52:10 +0900

    Feat: 이미지 업로드 공통 컴포넌트 구현

--- src/components/common/ImageUploader.tsx
+++ src/components/common/ImageUploader.tsx
@@ -4,46 +4,55 @@ import { useRef } from "react";
 import cancelIcon from "/public/images/cancel.svg";
 import { v4 as uuidv4 } from "uuid";
 
+interface ImageObject {
+  file: File;
+  url: string;
+}
+
 interface ImageUploaderProps {
   options: string;
   options2: string;
-  selectedImgs: File[];
-  previewImages: string[];
-  setPreviewImages: React.Dispatch<React.SetStateAction<string[]>>;
-  setSelectedImgs: React.Dispatch<React.SetStateAction<File[]>>;
+  selectedImages: File[];
+  previewImages: ImageObject[];
+  setPreviewImages: React.Dispatch<React.SetStateAction<object[]>>;
+  setSelectedImages: React.Dispatch<React.SetStateAction<File[]>>;
 }
 
 export default function ImageUploader({
   options,
   options2,
-  selectedImgs,
+  selectedImages,
   previewImages,
   setPreviewImages,
-  setSelectedImgs,
+  setSelectedImages,
 }: ImageUploaderProps) {
   const upload = useRef<HTMLInputElement>(null);
-
   const handleImgClick = () => {
     if (upload.current?.files?.[0]) {
-      const url = upload.current.files[0];
-      const previewImage = URL.createObjectURL(upload.current.files[0]);
-      // console.log(url)
-      console.log(previewImages);
-      setPreviewImages(prev => [...prev, previewImage]);
-      setSelectedImgs(prev => [...prev, url]);
-      // console.log(selectedImgs);
+      if (Array.from(upload.current.files).length + previewImages.length > 5) {
+        alert("이미지는 다섯개까지 올릴 수 있습니다.");
+      } else {
+        Array.from(upload.current.files).forEach(file => {
+          const url = URL.createObjectURL(file);
+          setPreviewImages(prev => [...prev, { file, url }]);
+          setSelectedImages(prev => [...prev, file]);
+        });
+      }
     }
   };
 
-  const deleteImg = (targetFileName: string) => {
-    setPreviewImages(previewImages.filter(file => file !== targetFileName));
+  const deleteImg = (target: { file: File; url: string }) => {
+    setPreviewImages(previewImages.filter(item => item.url !== target.url));
+    setSelectedImages(selectedImages.filter(file => file !== target.file));
   };
 
   return (
     <div className={`flex gap-[5px] ${options2}`}>
       <label
         htmlFor="file-uploader"
-        className={`w-[63px] cursor-pointer ${options}`}
+        className={`w-[63px] ${
+          selectedImages.length >= 5 ? "" : "cursor-pointer"
+        } ${options}`}
       >
         <Image
           src={uploadIcon}
@@ -61,19 +70,19 @@ export default function ImageUploader({
         onChange={handleImgClick}
         multiple
         style={{ display: "none" }}
-        disabled={selectedImgs.length === 5 ? true : false}
+        disabled={selectedImages.length >= 5 ? true : false}
       />
       <div className="flex gap-[5px]">
-        {previewImages.map(file => (
+        {previewImages.map(item => (
           <div className="relative" key={uuidv4()}>
             <img
-              src={file}
+              src={item.url}
               alt="selectedImg"
               className="w-[63px] h-[61px] rounded-[10px]"
             />
             <Image
               src={cancelIcon}
-              onClick={() => deleteImg(file)}
+              onClick={() => deleteImg(item)}
               alt="cancelIcon"
               className="absolute top-[2px] right-[2px]"
             />
