81f3de6ff3d594fc6baf6ffc9a7cc6899cfc23e5
energy1-914 <dlawldnjs5@daum.net>
2023-08-01 15:07:23 +0900

    Fix: ImageUploader 파일 프리뷰 이미지 배열 추가

--- src/components/common/ImageUploader.tsx
+++ src/components/common/ImageUploader.tsx
@@ -7,28 +7,36 @@ import { v4 as uuidv4 } from "uuid";
 interface ImageUploaderProps {
   options: string;
   options2: string;
-  selectedImgs: string[];
-  setSelectedImgs: React.Dispatch<React.SetStateAction<string[]>>
+  selectedImgs: File[];
+  previewImages: string[];
+  setPreviewImages: React.Dispatch<React.SetStateAction<string[]>>;
+  setSelectedImgs: React.Dispatch<React.SetStateAction<File[]>>;
 }
 
 export default function ImageUploader({
   options,
   options2,
   selectedImgs,
+  previewImages,
+  setPreviewImages,
   setSelectedImgs,
 }: ImageUploaderProps) {
   const upload = useRef<HTMLInputElement>(null);
 
   const handleImgClick = () => {
     if (upload.current?.files?.[0]) {
-      const url = URL.createObjectURL(upload.current.files[0]);
+      const url = upload.current.files[0];
+      const previewImage = URL.createObjectURL(upload.current.files[0]);
+      // console.log(url)
+      console.log(previewImages);
+      setPreviewImages(prev => [...prev, previewImage]);
       setSelectedImgs(prev => [...prev, url]);
-      console.log(selectedImgs);
+      // console.log(selectedImgs);
     }
   };
 
-  const deleteImg = (targetImg: string) => {
-    setSelectedImgs(selectedImgs.filter(img => img !== targetImg));
+  const deleteImg = (targetFileName: string) => {
+    setPreviewImages(previewImages.filter(file => file !== targetFileName));
   };
 
   return (
@@ -37,7 +45,13 @@ export default function ImageUploader({
         htmlFor="file-uploader"
         className={`w-[63px] cursor-pointer ${options}`}
       >
-        <Image src={uploadIcon} alt="upload-image" priority={true} />
+        <Image
+          src={uploadIcon}
+          alt="upload-image"
+          priority={true}
+          width={63}
+          height={61}
+        />
       </label>
       <input
         id="file-uploader"
@@ -45,21 +59,21 @@ export default function ImageUploader({
         accept="image/*"
         ref={upload}
         onChange={handleImgClick}
-        multiple={true}
+        multiple
         style={{ display: "none" }}
         disabled={selectedImgs.length === 5 ? true : false}
       />
       <div className="flex gap-[5px]">
-        {selectedImgs.map(img => (
+        {previewImages.map(file => (
           <div className="relative" key={uuidv4()}>
             <img
-              src={img}
+              src={file}
               alt="selectedImg"
               className="w-[63px] h-[61px] rounded-[10px]"
             />
             <Image
               src={cancelIcon}
-              onClick={() => deleteImg(img)}
+              onClick={() => deleteImg(file)}
               alt="cancelIcon"
               className="absolute top-[2px] right-[2px]"
             />
