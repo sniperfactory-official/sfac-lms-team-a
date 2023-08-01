865234c7485230fc7c7e1040d29afd797a2559e3
seonnn <ssni0105@gmail.com>
2023-08-02 00:22:50 +0900

    Fix: React 중복 import 에러 해결(중복 import 코드 삭제)

--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import close from "/public/images/close.svg";
+
 interface ModalWrapperProps {
   modalTitle?: React.ReactNode;
   onCloseModal: () => void;
