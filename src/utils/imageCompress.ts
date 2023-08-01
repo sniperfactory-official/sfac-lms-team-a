8f00591fe80300441b585922beb2f032ca9fe9e6
energy1-914 <dlawldnjs5@daum.net>
2023-08-02 02:58:04 +0900

    Feat: 이미지 압축 기능 구현

---
+++ src/utils/imageCompress.ts
@@ -0,0 +1,20 @@
+import imageCompression from "browser-image-compression";
+
+interface ImageCompressionProps {
+  file: File;
+  setCompressedImages: React.Dispatch<React.SetStateAction<File[]>>;
+}
+
+export default async function imageCompress({file, setCompressedImages}: ImageCompressionProps) {
+  const options = {
+    maxSizeMB: 0.2, // 이미지 최대 용량 - 200kb
+    maxWidthOrHeight: 1920, // 최대 넓이(혹은 높이)
+    useWebWorker: true,
+  };
+  try {
+    const compressedFile = await imageCompression(file, options);
+    setCompressedImages(prev => [...prev, compressedFile]);
+  } catch (error) {
+    console.log(error);
+  }
+}
