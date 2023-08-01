36726cdd46033baef671cbefbd97645e66fd3375
Jin <jinnkimm7@gmail.com>
2023-08-02 01:55:37 +0900

    Feat: 게시글이 존재하지 않을 경우

--- src/components/Community/CommunityList.tsx
+++ src/components/Community/CommunityList.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import CommunityCard from "@/components/Card/CommunityCard";
 import useGetSelectedPost from "@/hooks/reactQuery/useGetSelectedPost";
 import Aside from "./Aside/Aside";
+import Image from "next/image";
 
 const CommunityList = () => {
   const [activeCategory, setActiveCategory] = useState<string>("");
@@ -23,21 +24,31 @@ const CommunityList = () => {
   return (
     <div className="ml-[470px]">
       <Aside onCategorySelect={setActiveCategory} />
-      {postList?.map(data => (
-        <CommunityCard
-          id={data.id}
-          userId={data.userId}
-          parentId={data.parentId}
-          key={data.id}
-          user={data.user}
-          createdAt={data.createdAt}
-          postImages={data.postImages}
-          title={data.title}
-          content={data.content}
-          thumbnailImages={data.thumbnailImages}
-          tags={data.tags}
+      {postList?.length !== 0 ? (
+        postList?.map(data => (
+          <CommunityCard
+            id={data.id}
+            userId={data.userId}
+            parentId={data.parentId}
+            key={data.id}
+            user={data.user}
+            createdAt={data.createdAt}
+            postImages={data.postImages}
+            title={data.title}
+            content={data.content}
+            thumbnailImages={data.thumbnailImages}
+            tags={data.tags}
+          />
+        ))
+      ) : (
+        <Image
+          src="/images/noPostingMessage.svg"
+          width={573}
+          height={199}
+          alt="게시글이 존재하지 않습니다."
+          className="absolute top-[199px] left-[531px]"
         />
-      ))}
+      )}
     </div>
   );
 };
