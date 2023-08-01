776b6d8c992b5709ed130d76b1244fbaeae6dcc5
Jin <jinnkimm7@gmail.com>
2023-08-02 00:51:50 +0900

    Update: CommunityList 기능 수정

--- src/components/Community/CommunityList.tsx
+++ src/components/Community/CommunityList.tsx
@@ -1,53 +1,12 @@
 import React, { useState } from "react";
-import Aside from "@/components/Aside/Aside";
 import CommunityCard from "@/components/Card/CommunityCard";
 import useGetSelectedPost from "@/hooks/reactQuery/useGetSelectedPost";
-
-const TEST_DATA = [
-  {
-    category: "필독",
-    title: "안녕",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ",
-    tags: ["자바", "플러터", "자스"],
-    commentCount: 4,
-  },
-  {
-    category: "안내사항",
-    title: "하이",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ",
-    tags: ["스프링", "리액트", "뷰"],
-    commentCount: 0,
-  },
-  {
-    category: "질문있어요",
-    title: "올라",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ",
-    tags: ["스프링", "리액트", "뷰"],
-    commentCount: 7,
-  },
-  {
-    category: "자유게시판",
-    title: "봉주르",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ",
-    tags: ["스프링", "리액트", "뷰"],
-    commentCount: 5,
-  },
-  {
-    category: "익명 피드백",
-    title: "곤니찌와",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into  ",
-    tags: ["스프링", "리액트", "뷰"],
-    commentCount: 3,
-  },
-];
+import Aside from "./Aside/Aside";
 
 const CommunityList = () => {
   const [activeCategory, setActiveCategory] = useState<string>("");
+
+  // 게시물 리스트
   const {
     data: postList,
     isLoading,
@@ -66,11 +25,17 @@ const CommunityList = () => {
       <Aside onCategorySelect={setActiveCategory} />
       {postList?.map(data => (
         <CommunityCard
-          key={data.title}
+          id={data.id}
+          userId={data.userId}
+          parentId={data.parentId}
+          key={data.id}
+          user={data.user}
+          createdAt={data.createdAt}
+          postImages={data.postImages}
           title={data.title}
           content={data.content}
+          thumbnailImages={data.thumbnailImages}
           tags={data.tags}
-          commentCount={3}
         />
       ))}
     </div>
