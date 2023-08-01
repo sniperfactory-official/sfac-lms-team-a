b2d12b73a9ea9b06c4886d961ec1b6fc58c9cd74
minzyee <vvzmsqufvv@gmail.com>
2023-08-01 22:45:33 +0900

    Refactor: 강의 목록 데이터 패칭 쿼리 훅 수정

--- src/hooks/reactQuery/lecture/useGetAllLectureListQuery.ts
+++ src/hooks/reactQuery/lecture/useGetAllLectureListQuery.ts
@@ -6,33 +6,24 @@ import { collection, getDocs } from "firebase/firestore";
 const getAllLetureData = async () => {
   const q = await getDocs(collection(db, "lectures"));
   const lectureInfoDataList = q.docs.map(doc => doc.data());
-  return lectureInfoDataList;
-};
+  console.log("전체 강의 목록(lectureInfoDataList): ", lectureInfoDataList);
 
-const useGetAllLectureListQuery = (selectedCourseId: string) => {
-  const { data: allLecturesData, isLoading: lectureLoading } = useQuery(
-    ["lectures"],
-    async () => await getAllLetureData(),
-    {
-      refetchOnWindowFocus: false,
-    },
-  );
-
-  // course 컬렉션의 id 필드별로 배열로 묶기
   const lecturesByCourseId: { [key: string]: Lecture[] } = {};
-
-  // lectureItem은 강의 목록들이다.
-  allLecturesData?.forEach((lectureItem: any) => {
-    const courseId = lectureItem.courseId.id; // I7YsTuxOWvT1M2lakkAM
+  console.log("lecturesByCourseId:::: ", lecturesByCourseId);
+  lectureInfoDataList.forEach((lectureItem: any) => {
+    const courseId = lectureItem.courseId.id; // course의 id에 접근
     if (!lecturesByCourseId[courseId]) {
       lecturesByCourseId[courseId] = [];
     }
     lecturesByCourseId[courseId].push(lectureItem);
-
-    // console.log("lecturesByCourseId[courseId]: ", lecturesByCourseId[courseId]); // 강의 목록이 잘 들어가있다.
-    // console.log("코스별로묶은 lecturesByCourseId 객체: ", lecturesByCourseId); // 객체 안에 course id 별로 강의 리스트 넣어둠.
   });
-  return { allLecturesData, lectureLoading, lecturesByCourseId };
+
+  console.log("course의 id를 key로 기준으로한 배열:: ", lecturesByCourseId);
+  return lecturesByCourseId;
+};
+
+const useGetAllLectureListQuery = () => {
+  return useQuery(["lectures"], async () => await getAllLetureData());
 };
 
 export default useGetAllLectureListQuery;
