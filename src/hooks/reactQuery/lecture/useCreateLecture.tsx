921a8ea79c5f6eac1be1815c340b6b6d55001f09
Majesty-jun <hkwns01@gmail.com>
2023-08-02 15:47:39 +0900

    Feat: firebase 강의 생성 커스텀 훅 완성

--- src/hooks/reactQuery/lecture/useCreateLecture.tsx
+++ src/hooks/reactQuery/lecture/useCreateLecture.tsx
@@ -2,36 +2,47 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { db } from "@/utils/firebase";
 import { addDoc, collection, doc, getDoc } from "firebase/firestore";
 import { CreateLecture } from "@/app/classroom/(components)/CreateLecture";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 const createLecture = async (lecture: CreateLecture) => {
-  const courseDocSnap = await getDoc(doc(db, "courses", lecture.courseId));
-  const userDocSnap = await getDoc(doc(db, "users", lecture.userId));
-  console.log(courseDocSnap, userDocSnap);
-  // const docRef = await addDoc(collection(db, `lecture`), { ...lecture });
-  // const storage = getStorage();
-  // const storageRef = ref(storage, 'lectures/videos');
-  // uploadBytes(storageRef, lecture.lectureContent.videoUrl);
-  URL.createObjectURL;
-  // console.log(docRef);
+  let videoUrl = "";
+
+  if (lecture.lectureType === "비디오") {
+    const storage = getStorage();
+    const storageRef = ref(
+      storage,
+      "lectures/videos/" + lecture.lectureContent.video[0].name,
+    );
+    await uploadBytes(storageRef, lecture.lectureContent.video[0]);
+    videoUrl = await getDownloadURL(
+      ref(storage, "lectures/videos/" + lecture.lectureContent.video[0].name),
+    );
+  }
+
+  const lectureData = {
+    ...lecture,
+    user: doc(db, "courses", lecture.courseId),
+    course: doc(db, "users", lecture.userId),
+    lectureContent: {
+      ...lecture.lectureContent,
+      video: videoUrl,
+    },
+  };
+  const docRef = await addDoc(collection(db, `lectures`), lectureData);
+  console.log(docRef.id);
 };
 
-const useCreateLecture = (lecture: CreateLecture) => {
+const useCreateLecture = (modalOpenHandler: () => void) => {
   const queryClient = useQueryClient();
-  const { mutate, isError, isLoading, isSuccess } = useMutation<
-    void,
-    Error,
-    CreateLecture
-  >(() => createLecture(lecture), {
-    onSuccess: data => {
+  return useMutation<void, Error, CreateLecture>(createLecture, {
+    onSuccess: () => {
       queryClient.invalidateQueries(["lecture"]);
+      modalOpenHandler();
     },
     onError: error => {
       console.log(error);
     },
   });
-
-  return { mutate, isError, isLoading, isSuccess };
 };
 
 export default useCreateLecture;
