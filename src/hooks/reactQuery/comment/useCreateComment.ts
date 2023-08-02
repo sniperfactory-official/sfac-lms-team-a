8cf124a2b5c763b42db6003f4006c5c5f5e802e9
lgrin-byte <lgrin02@naver.com>
2023-08-02 12:24:11 +0900

    Fix: 댓글 작성시 새로고침 없이 반영

--- src/hooks/reactQuery/comment/useCreateComment.ts
+++ src/hooks/reactQuery/comment/useCreateComment.ts
@@ -5,8 +5,7 @@ import {
   DocumentReference,
 } from "firebase/firestore";
 import { db } from "@/utils/firebase";
-import { useMutation } from "@tanstack/react-query";
-import { Post } from "@/types/firebase.types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Write = {
   parentId: string;
@@ -31,6 +30,7 @@ const createComment = async ({ post }: createCommentProps) => {
 
 const useCreateComment = () => {
   //reset: 작성후 초기화 작업
+  const queryClient = useQueryClient();
   const { mutate, data, error, reset } = useMutation<
     void,
     Error,
@@ -44,6 +44,8 @@ const useCreateComment = () => {
     },
     onSuccess: (data, variables, context) => {
       // Optional: 입력 성공시 메세지 UI 업데이트 하고 싶은 경우 사용
+      queryClient.invalidateQueries(["comment"]);
+
       console.log(data);
     },
   });
