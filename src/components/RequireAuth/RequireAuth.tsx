14e25fff351577e75a6822403bc3a7ecbaf61287
energy1-914 <dlawldnjs5@daum.net>
2023-08-02 16:15:44 +0900

    Chore: conflict 해결하면서 사라진 파일 및 코드 복구

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,19 +1,22 @@
-// import Navbar from "@/components/Header/Navbar";
+'use client'
+import Navbar from "@/components/Header/Navbar";
 import Tab from "@/components/Header/Tab";
 import Footer from "@/components/Footer/Footer";
 import { auth } from "@/utils/firebase";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import LoadingSpinner from "../Loading/Loading";
-
+import {useAppSelector} from "@/redux/store";
 export default function RequireAuth({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   const router = useRouter();
   const [authenticated, setAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
@@ -24,35 +27,43 @@ export default function RequireAuth({
       }
       setLoading(false);
     });
-
+    
+    // unmounted 됐을 때 메모리 누수의 문제가 발생할 수 있으니 cleanup()함수 사용
     return () => {
       unsubscribe();
-    };
+    }
   }, []);
 
   useEffect(() => {
-    // 로그인이 되지 않았거나
+    // 로그인이 되지 않았거나 로딩이 안 돼면 계속 로그인 페이지
     if (!loading && !authenticated) {
       router.push("/");
     } else {
-      router.push("/community");
+    // 성공시 커뮤니티 페이로 이동
+      router.push("/community")
     }
   }, [loading, authenticated]);
-
+  
+  // 로딩 상태면 Loading Spinner 사용
   if (loading) {
-    <LoadingSpinner />;
+    <LoadingSpinner />
   } else {
     if (authenticated) {
       return (
         <>
-          {/* <Navbar /> */}
+          <Navbar />
           <Tab />
           {children}
           <Footer />
         </>
-      );
+      )
     } else {
-      return <>{children}</>;
+      return (
+        <>
+          {children}
+        </>
+      )
     }
   }
+
 }
