d84d5a4338ac26a75cdbc6f6fdcd29b2c26a9545
energy1-914 <dlawldnjs5@daum.net>
2023-08-02 14:56:43 +0900

    Feat: 글 업로드 mutation 쿼리 생성

---
+++ src/hooks/reactQuery/post/useCreatePostMutation.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/utils/firebase";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { PostValue } from "@/components/Community/PostForm";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const createPost = async (formData: PostValue) => {
+  const docRef = await addDoc(collection(db, "posts"), {
+    parentId: "",
+    category: formData.category,
+    userId: "",
+    title: formData.title,
+    content: formData.content,
+    postImages: formData.postImages,
+    thumbnailImages: formData.thumbnailImages,
+    tags: formData.tags,
+    createdAt: formData.createdAt,
+  });
+  console.log('docRef', docRef)
+};
+
+export const usePostMutation = () => {
+  const queryClient = useQueryClient();
+  const { mutate, isLoading } = useMutation(createPost
+    , {
+    onSuccess: () => {
+      console.log('onSuccess !!!')
+    }
+  }
+  );
+
+  return { mutate, isLoading  };
+};
