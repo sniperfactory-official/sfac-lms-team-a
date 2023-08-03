e588a6d748a14467f11837ea06a9e4d6b0e92375
energy1-914 <dlawldnjs5@daum.net>
2023-08-03 20:51:25 +0900

    Refactor: 카테고리 목록 중 빅테이터 항목 삭제

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -2,7 +2,6 @@ const CATEGORY_DATA = [
   { icon: "🎯", category: "필독" },
   { icon: "🔊", category: "안내사항" },
   { icon: "👋", category: "질문있어요" },
-  { icon: "📊", category: "BigData" },
   { icon: "🔥", category: "자유게시판" },
   { icon: "🔓", category: "익명 피드백" },
 ];
