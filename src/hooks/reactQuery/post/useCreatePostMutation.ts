dbf394db6af2b5e54fdc9685b7a528a5dbd0ee2f
energy1-914 <dlawldnjs5@daum.net>
2023-08-03 20:50:37 +0900

    Fix: 커뮤니티 페이지 글 제출 시 firebase에 등록 수정(userId 추가)

--- src/hooks/reactQuery/post/useCreatePostMutation.ts
+++ src/hooks/reactQuery/post/useCreatePostMutation.ts
@@ -1,31 +1,28 @@
 import { db } from "@/utils/firebase";
-import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { addDoc, collection, DocumentReference } from "firebase/firestore";
 import { PostValue } from "@/components/Community/PostForm";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useAppSelector } from "@/redux/store";
+import { useMutation } from "@tanstack/react-query";
 
-export const createPost = async (formData: PostValue) => {
-  const userId = useAppSelector(state => state.userInfo.id);
-  const docRef = await addDoc(collection(db, "posts"), {
+interface CreatePostProps {
+  data: PostValue;
+  userRef: DocumentReference;
+}
+
+export const createPost = async ({ data, userRef }: CreatePostProps) => {
+  await addDoc(collection(db, "posts"), {
     parentId: "",
-    category: formData.category,
-    userId: userId,
-    title: formData.title,
-    content: formData.content,
-    postImages: formData.postImages,
-    thumbnailImages: formData.thumbnailImages,
-    tags: formData.tags,
-    createdAt: formData.createdAt,
+    category: data.category,
+    userId: userRef,
+    title: data.title,
+    content: data.content,
+    postImages: data.postImages,
+    thumbnailImages: data.thumbnailImages,
+    tags: data.tags,
+    createdAt: data.createdAt,
   });
 };
 
-export const usePostMutation = () => {
-  const queryClient = useQueryClient();
-  const { mutate, isLoading } = useMutation(createPost, {
-    onSuccess: () => {
-      console.log("onSuccess !!!");
-    },
-  });
-
-  return { mutate, isLoading };
+export const usePostMutation = options => {
+  return useMutation(createPost, {
+    ...options });
 };
