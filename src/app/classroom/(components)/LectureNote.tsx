159f4abecfa6df1ce75b405e2c6cc23cf1be139e
Majesty-jun <hkwns01@gmail.com>
2023-08-03 15:53:48 +0900

    Feat: MDEditor 이미지 추가 기능 구현

--- src/app/classroom/(components)/LectureNote.tsx
+++ src/app/classroom/(components)/LectureNote.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { CreateLecture } from "./CreateLecture";
 import MDEditor from "@uiw/react-md-editor";
+import uploadFileToStorage from "@/utils/uploadFileToStorage";
 
 interface Props {
   setLecture: React.Dispatch<React.SetStateAction<CreateLecture>>;
@@ -22,9 +23,36 @@ export default function LectureNote({ setLecture }: Props) {
     }
   }, [setLecture, text]);
 
+  const onChangeFile = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    try {
+      const imageUrl = await uploadFileToStorage("lectures/noteImages/", file);
+      setText(prev => prev + `\n![Alt Text](${imageUrl})`);
+    } catch (error) {
+      console.error("Failed to upload Image", error);
+    } finally {
+      e.target.value = "";
+    }
+  };
+
   return (
     <div className="container" data-color-mode="light">
       <MDEditor value={text} onChange={setText} height={300} />
+      <label
+        htmlFor="editorImage"
+        className="mt-[12px] flex justify-center items-center w-[115px] h-[35px] rounded-[10px] bg-primary-80 text-white cursor-pointer"
+      >
+        사진 추가
+      </label>
+      <input
+        id="editorImage"
+        className="sr-only"
+        type="file"
+        accept="image/*"
+        onChange={onChangeFile}
+      />
     </div>
   );
 }
