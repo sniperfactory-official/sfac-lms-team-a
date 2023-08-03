f1ac8e138778153c7af123883506cd46be7fc36a
energy1-914 <dlawldnjs5@daum.net>
2023-08-03 15:50:29 +0900

    Refactor: 카테고리 데이터 항목 이름 변경

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,9 +1,9 @@
 const CATEGORY_DATA = [
-  { icon: "🎯", text: "필독" },
-  { icon: "🔊", text: "안내사항" },
-  { icon: "👋", text: "질문있어요" },
-  { icon: "📊", text: "BigData" },
-  { icon: "🔥", text: "자유게시판" },
-  { icon: "🔓", text: "익명 피드백" },
+  { icon: "🎯", category: "필독" },
+  { icon: "🔊", category: "안내사항" },
+  { icon: "👋", category: "질문있어요" },
+  { icon: "📊", category: "BigData" },
+  { icon: "🔥", category: "자유게시판" },
+  { icon: "🔓", category: "익명 피드백" },
 ];
 export default CATEGORY_DATA;
