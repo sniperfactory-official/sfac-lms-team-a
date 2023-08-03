848563789c62616a0f3a33a8c3e25855db929e84
lgrin-byte <lgrin02@naver.com>
2023-08-03 12:56:57 +0900

    Feat: 태그 기능 구현 및 타입 에러 수정

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,6 +1,23 @@
+"use client";
 
 import Layout from "@/components/Community/Layout";
+import CommunityModal from "@/components/CommunityModal/CommunityModal";
+import ModalWrapper from "@/components/ModalWrapper";
+import { useState } from "react";
 
 export default function CommunityPage() {
-  return <Layout />;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const onCloseModal = () => setIsModalOpen(!isModalOpen);
+
+  return (
+    <div>
+      {isModalOpen && (
+        <ModalWrapper
+          modalTitle="상세보기"
+          onCloseModal={onCloseModal}
+          children={<CommunityModal />}
+        />
+      )}
+    </div>
+  );
 }
