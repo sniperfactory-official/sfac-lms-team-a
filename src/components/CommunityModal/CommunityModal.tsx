343b4096aa0c8b7df091636b8e1c02f94379d149
energy1-914 <dlawldnjs5@daum.net>
2023-08-07 14:09:47 +0900

    Refactor: 콘솔 삭제 및 Image 속성 추가

--- src/components/CommunityModal/CommunityModal.tsx
+++ src/components/CommunityModal/CommunityModal.tsx
@@ -22,11 +22,8 @@ interface NestedId {
 }
 
 export default function CommunityModal() {
-  // const postId = "YiJVx6OQBhlGGRCUj1WU";
   const postId = useAppSelector(state => state.postInfo.postId);
-  // const userId="HNjyTagV4fgalY53duWJSS5ugdw1"
   const userId = useAppSelector(state => state.userInfo.id);
-  // console.log(userId2);
 
   // 이미지 모달
   const [isImageModalOn, setIsImageModalOn] = useState(false);
