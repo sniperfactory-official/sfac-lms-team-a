4a342542be3e26c6f03f86fb46d05da6a57e65fc
minzyee <vvzmsqufvv@gmail.com>
2023-08-07 15:37:46 +0900

    Feat: 강의 리스트 DnD 기능 적용

--- src/app/classroom/(components)/CourseSection.tsx
+++ src/app/classroom/(components)/CourseSection.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Sidebar from "@/components/Sidebar";
+import useUpdateLectureOrder from "@/hooks/reactQuery/lecture/useUpdateLectureOrder";
 import { Lecture } from "@/types/firebase.types";
 
 const CourseSection = ({
@@ -15,11 +16,33 @@ const CourseSection = ({
   onClickedCourse,
 }: any) => {
   const [isCheck, setIsChecked] = useState(false);
+  const [lectureListItem, setLectureListItem] = useState([]);
+
+  // 강의 순서를 변경하는 뮤테이션 훅
+  const { mutateAsync: updateMutate } = useUpdateLectureOrder();
+
   useEffect(() => {
-    const result = courseChecked.includes(courseData.id);
-    console.log("체크 결과", result);
+    const result = courseChecked.includes(courseData.id); // true, false
+    const updateLectureList = allLecturesData.map((lecture: Lecture) => {
+      return {
+        id: lecture.id,
+        title: lecture.title,
+        checked: checkedLectureIds.includes(lecture.id),
+      };
+    });
+
     setIsChecked(result);
-  }, [courseChecked, courseData]);
+    setLectureListItem(updateLectureList);
+  }, [courseChecked, courseData, allLecturesData, checkedLectureIds]);
+
+  const onDragEndTest = (movedLectureItem: any) => {
+    setLectureListItem(movedLectureItem);
+    // 드래그 앤 드롭으로 변경된 순서를 Firebase에 저장한다.
+    updateMutate(movedLectureItem);
+    console.log("movedLectureItem!!: ", movedLectureItem);
+  };
+
+  console.log("lectureListItem::", lectureListItem);
 
   return (
     <button onClick={() => onClickedCourse(courseData.id)} className="mb-2">
@@ -27,19 +50,12 @@ const CourseSection = ({
         key={courseData.id}
         courseId={courseData.id}
         header={courseData.title}
-        contents={
-          allLecturesData.map((lecture: Lecture) => {
-            return {
-              id: lecture.id,
-              title: lecture.title,
-              checked: checkedLectureIds.includes(lecture.id),
-            };
-          }) || []
-        }
+        contents={lectureListItem}
         isEdit={isEdit}
         lectureCheckHandler={onLectureCheck} // 각 강의 항목 체크 여부
         isCourseChecked={isCheck} // 강의 중 하나라도 체크가 해제될 시, 섹션도 체크 해제 됨(코스의 체크 여부)
         courseCheckHandler={onCourseCheck} // 코스 섹션 체크 토글
+        onDragEnd={onDragEndTest}
       />
     </button>
   );
