8fc55189123a7bcf0c1c9ecb63102ca1882e1ad3
energy1-914 <dlawldnjs5@daum.net>
2023-08-07 14:12:05 +0900

    Fix: PostForm 제출시 로직 변경 및 게시글 수정 시 로직 추가

--- src/components/Community/PostForm/PostForm.tsx
+++ src/components/Community/PostForm/PostForm.tsx
@@ -12,7 +12,8 @@ import { useUpdatePostMutation } from "@/hooks/reactQuery/community/useUpdatePos
 import useGetProfileImage from "@/hooks/reactQuery/community/useGetProfileImage";
 import useGetSelectedPost from "@/hooks/reactQuery/useGetSelectedPost";
 import useGetPostImage from "@/hooks/reactQuery/community/useGetPostImage";
-import { uploadStorageImages } from "@/utils/uploadStorageImages";
+import uploadStorageImages from "@/utils/uploadStorageImages";
+import deleteStorageImages from "@/utils/deleteStorageImages";
 import imageCompress from "@/utils/imageCompress";
 import { db } from "@/utils/firebase";
 import { Timestamp, doc } from "firebase/firestore";
@@ -46,20 +47,16 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
   const contentValue = watch("content");
   const [selectedCategory, setSelectedCategory] = useState<string>("");
   const [tagList, setTagList] = useState<string[]>([]);
-  //{File, url} 자체들이 담긴 배열
+  // {File, url, status} 가 담긴 배열
   const [selectedImages, setSelectedImages] = useState<ImageObject[]>([]);
-  // url이 담긴 배열
-  const [previewImages, setPreviewImages] = useState<string[]>([]);
-  //압축된 이미지  배열
-  const [compressedImages, setCompressedImages] = useState<File[]>([]);
   const [icon, setIcon] = useState("");
-  // 수정 시 기존 썸네일 배열
+  // 수정 시 기존 이미지/썸네일 배열
   const [postedThumbnailImages, setPostedThumbnailImages] = useState<string[]>(
     [],
   );
+
   let submitImages: string[] = [];
   let submitThumbnailImages: string[] = []; // 폼데이터에 제출할 url들 담긴 배열
-
   const userId = useAppSelector(state => state.userInfo.id);
   const userProfile = useAppSelector(state => state.userInfo.profileImage);
   const userName = useAppSelector(state => state.userInfo.username);
@@ -72,24 +69,17 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
 
   const userRef = doc(db, "users", userId);
 
-  const getRoot = (targetArray: string[], originArray: ImageObject[]): void => {
-    originArray.map(target =>
-      targetArray.push(`posts/postImages/${target.file.name}`),
-    );
-  };
   const cleanup = () => {
-    previewImages.map(item => URL.revokeObjectURL(item));
+    selectedImages
+      .filter(item => item.status === "new")
+      .map(item => URL.revokeObjectURL(item.url));
     setSelectedImages([]);
-    setPreviewImages([]);
-    setCompressedImages([]);
-    console.log("clean!");
     onCleanup && onCleanup();
   };
   const getCurrentTime = Timestamp.now();
 
   const { mutate: postMutate, isLoading: postLoading } = useCreatePostMutation({
     onSuccess: () => {
-      onClose();
       cleanup();
     },
   });
@@ -97,7 +87,6 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
   const { mutate: updateMutate, isLoading: updateLoading } =
     useUpdatePostMutation({
       onSuccess: () => {
-        onClose();
         cleanup();
       },
     });
@@ -120,52 +109,106 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
 
   // 글을 디비에서 불러와서 나타낸다.
   useEffect(() => {
-    console.log("postedData:: ", postedData);
     if (!postLoading && postId && postedData) {
       setValue("title", postedData.title);
       setValue("content", postedData.content);
       setTagList(postedData.tags);
       setIcon(
         CATEGORY_DATA.filter(item => item.category === postedData.category)[0]
-          .icon,
+          ?.icon,
       );
-      if (postedData.thumbnailImages) {
+      const isExistsImageData = selectedImages.some(
+        item => imageData?.includes(item.url),
+      );
+      if (imageData && !isExistsImageData) {
         setPostedThumbnailImages(postedData.thumbnailImages);
-        setPreviewImages(prev => [...prev, ...postedThumbnailImages]);
-        console.log(postedThumbnailImages);
+        imageData.map(url => {
+          const urlObject = new URL(url);
+          const pathParts = urlObject.pathname.split("/");
+          const fileRoot = pathParts[pathParts.length - 1].replaceAll(
+            "%2F",
+            "/",
+          );
+          setSelectedImages(prev => [
+            ...prev,
+            { url: url, status: "existing", root: fileRoot },
+          ]);
+        });
       }
     }
-  }, [postId]);
+  }, [postId, postedData, imageData]);
 
-  if (postLoading || profileLoading || postedLoading || updateLoading)
+  if (
+    postLoading ||
+    profileLoading ||
+    postedLoading ||
+    updateLoading ||
+    imageLoading
+  )
     return <LoadingSpinner />;
 
   const onSubmit = handleSubmit(async data => {
-    // 이미지를 압축한다
-    const compressedImagesPromises = selectedImages.map(item => imageCompress(item.file));
-    const compressedImagesArray = await Promise.all(compressedImagesPromises);
-    setCompressedImages(compressedImagesArray);
-
-    //경로에 맞게 배열에 경로를 포함한 파일루트를 담아준다.
-    getRoot(submitImages, selectedImages);
-    let copy = [...submitImages];
-    submitThumbnailImages = copy.map(item =>  
-      item.replace("postImages", "thumbnailImages"),
+    const newImages = selectedImages.filter(item => item.status === "new");
+    const deletedImages = selectedImages.filter(
+      item => item.status === "deleted",
     );
+    const existingImages = selectedImages.filter(
+      item => item.status === "existing",
+    );
+
+    // 새로운 이미지 압축 & 스토리지 저장
+    if (newImages.length) {
+      const compressedImagesPromises = newImages.map(item =>
+        imageCompress(item.file),
+      );
+      const compressedImagesArray = await Promise.all(compressedImagesPromises);
+      const newThumbnailPaths = await uploadStorageImages(
+        "thumbnailImages",
+        compressedImagesArray,
+      );
+      const newPostImagePaths = await uploadStorageImages(
+        "postImages",
+        newImages.map(item => item.file),
+      );
+      submitImages.push(...newThumbnailPaths);
+      submitThumbnailImages.push(...newPostImagePaths);
+    }
+    // 기존 이미지 경로에 담는 로직
+    existingImages.forEach(item => {
+      submitImages.push(item.root);
+      submitThumbnailImages.push(
+        item.root.replace("postImages", "thumbnailImages"),
+      );
+    });
+
+    // 수정 시 삭제된 이미지 스토리지에서 제거
+
+    const targetRoots = deletedImages
+      .filter(item => postedThumbnailImages.includes(item.root))
+      .map(item => item.root);
+
+    // 기존 포스트에 있던 이미지 > 수정시 삭제된 이미지 : deleted
+    if (targetRoots.length) {
+      const targetThumbnailRoots = targetRoots.map(root =>
+        root.replace("postImages", "thumbnailImages"),
+      );
+      deleteStorageImages(targetRoots); // posts/postImages
+      deleteStorageImages(targetThumbnailRoots); // posts/ThumbnailImages
+    }
 
     // 데이터들을 직접 formdata에 담아준다.
     data.tags = tagList;
-    data.postImages = submitImages;
-    data.thumbnailImages = submitThumbnailImages;
-
-    // 이미지 & 압축 이미지를 스토리지에 저장한다.
-    uploadStorageImages("thumbnailImages", compressedImagesArray);
-    uploadStorageImages(
-      "postImages",
-      selectedImages.map(imageObject => imageObject.file),
-    );
+    // 이미지가 없을 경우 | 모두 삭제된 경우
+    if (postId && !newImages.length && !existingImages.length) {
+      data.postImages = [];
+      data.thumbnailImages = [];
+    } else {
+      data.postImages = submitImages;
+      data.thumbnailImages = submitThumbnailImages;
+    }
 
     // 게시글 등록 - 폼데이터를 파이어베이스에 저장한다.
+    onClose();
     if (postId) {
       data.updatedAt = getCurrentTime;
       updateMutate({ data, postId });
@@ -191,12 +234,13 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
         <input
           id="title"
           placeholder="제목을 입력해주세요. (선택)"
+          maxLength={15}
           {...register("title", { required: true })}
           className=" rounded-[8px] ring-inset ring-grayscale-10 focus:outline-none focus:ring-2 focus:primary-5"
         />
         <div className="relative">
           <textarea
-            className="w-full h-[300px] justify-center items-center rounded-[10px] border-grayscale-10 placeholder-grayscale-20 p-[15px] resize-none ring-1 ring-inset ring-grayscale-10 focus:outline-none focus:ring-2 focus:primary-5"
+            className="w-full h-[300px] justify-center items-center rounded-[10px] border-grayscale-10 placeholder-grayscale-20 p-[15px] pb-12 resize-none ring-1 ring-inset ring-grayscale-10 focus:outline-none focus:ring-2 focus:primary-5"
             placeholder="내용을 입력해주세요."
             {...register("content", { required: true })}
           />
@@ -205,8 +249,7 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
             options2="absolute start-[15px] bottom-[20px]"
             selectedImages={selectedImages}
             setSelectedImages={setSelectedImages}
-            previewImages={previewImages}
-            setPreviewImages={setPreviewImages}
+            postedThumbnailImages={postedThumbnailImages}
           />
           <input {...register("postImages")} type="hidden" />
           <input {...register("thumbnailImages")} type="hidden" />
@@ -229,9 +272,11 @@ export default function PostForm({ onClose, onCleanup }: PostFormProps) {
         </div>
         <Button
           text="업로드"
-          disabled={isSubmitting}
           isError={
             !titleValue || !contentValue || !selectedCategory ? true : false
+          }
+          disabled={
+            isSubmitting || !titleValue || !contentValue || !selectedCategory
           }
           options={"w-[115px] h-[35px] self-end"}
         />
