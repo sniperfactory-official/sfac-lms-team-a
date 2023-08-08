8b4be9f5f6fd820760ef8418af45778b2967a7ba
energy1-914 <dlawldnjs5@daum.net>
2023-08-08 13:20:33 +0900

    Feat: 마이페이지 모달창 초기작업 커밋

--- src/app/mypage/(components)/Category.tsx
+++ src/app/mypage/(components)/Category.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
-import arrow from "/public/images/Arrow.svg";
+import arrow from "/public/images/MypageArrow.svg";
 import { Timestamp } from "firebase/firestore";
+import { useAppDispatch } from "@redux/store";
+import { choicePost } from "@redux/postSlice";
 
 type CommentData = {
   id: string;
@@ -14,19 +16,34 @@ type CommentData = {
 type CategoryProps = {
   title: string;
   targetData: CommentData[] | undefined;
+  handleClick: () => void;
+  handleChoicePost: (id: string) => void;
 };
 
-export default function Category({ title, targetData }: CategoryProps) {
+export default function Category({
+  title,
+  targetData,
+  handleClick,
+  handleChoicePost,
+}: CategoryProps) {
+  // const dispatch = useAppDispatch();
+  // const  handleChoicePost = (id: string) => {
+  //     dispatch(choicePost({ postId: id, type: "detail" }));
+  // }
+
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col w-[275px]" onClick={handleClick}>
       <div className="flex justify-between">
         <h3 className="font-bold ">{title}</h3>
-        <Image src={arrow} width={10} height={14} alt="더보기 버튼" />
+        {title && (
+          <Image src={arrow} width={10} height={14} alt="더보기 버튼" />
+        )}
       </div>
       {targetData?.map(({ id, title, category, content }) => (
         <div
           key={id}
-          className="h-[73px] text-base border-solid border border-gray-200 rounded-[10px] px-[12px] py-[16px] my-3"
+          className="h-[73px] text-base border-solid border border-gray-200 rounded-[10px] px-[12px] py-[16px] my-3 cursor-pointer"
+          onClick={handleChoicePost(id)}
         >
           <div className="flex">
             <div className="align-middle px-[5px] leading-5 text-[10px] text-center bg-gray-200 rounded mr-[7px] mb-[4px]">
@@ -41,54 +58,4 @@ export default function Category({ title, targetData }: CategoryProps) {
       ))}
     </div>
   );
-}
-
-{
-  /* 
-      <div className="">
-        <div className="flex justify-between">
-          <h3 className="font-bold">{title}</h3>
-          <Image src={arrow} width={10} height={14} alt="더보기 버튼" />
-        </div>
-        {myPostData
-          ?.filter(el => el.category)
-          .map(post => (
-            <div className="  h-[73px] text-base border-solid border border-gray-200 rounded-[10px] px-[12px] py-[16px] my-3">
-              <div className="flex ">
-                <div className=" leading-5 px-[2px] text-[10px] bg-gray-200 rounded mr-[7px] mb-[4px]">
-                  {post.category}
-                </div>
-                <h4 className=" text-sm truncate ...">{post.title}</h4>
-              </div>
-              <p className=" text-xs text-primary-30 truncate overflow-hidden ...">
-                {post.content}
-              </p>
-            </div>
-          ))}
-      </div>
-
-      <div className="">
-        <div className="flex justify-between">
-          <h3 className="font-bold">{title}</h3>
-          <Image src={arrow} width={10} height={14} alt="더보기 버튼" />
-        </div>
-        {myPostData
-          ?.filter(el => !el.category)
-          .map(post => (
-            <div className="  h-[73px] text-base border-solid border border-gray-200 rounded-[10px] px-[12px] py-[16px] my-3">
-              <div className="flex ">
-                <div className=" leading-5 px-[2px] text-[10px] bg-gray-200 rounded mr-[7px] mb-[4px]">
-                  {post.parentData.category}
-                </div>
-                <h4 className=" text-sm truncate ...">
-                  {post.parentData.title}
-                </h4>
-              </div>
-              <p className=" text-xs text-primary-30 truncate overflow-hidden ...">
-                {post.content}
-              </p>
-            </div>
-          ))}
-      </div>
-    </div> */
 }
--- src/app/mypage/(components)/UserActivityList.tsx
+++ src/app/mypage/(components)/UserActivityList.tsx
@@ -1,23 +1,51 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Category from "./Category";
 import { useAppSelector } from "@/redux/store";
-import useGetProgressInfoQuery from "@/hooks/reactQuery/useGetProgressQuery";
 import useGetMyPosts from "@/hooks/reactQuery/mypage/useGetMyPosts";
-import useGetUserQuery from "@/hooks/reactQuery/navbar/useGetUserQuery";
 import useGetLectureComments from "@/hooks/reactQuery/mypage/useGetLectureComments";
 import LoadingSpinner from "@/components/Loading/Loading";
 import useGetAssignments from "@/hooks/reactQuery/mypage/useGetAssignments";
+import ModalWrapper from "@/components/ModalWrapper";
+import CommunityModal from "@/components/CommunityModal";
 
 export default function UserActivityList() {
+  const [isAssignmentsModalOpen, setIsAssignmentsModalOpen] = useState(false);
+  const [isPostModalOpen, setIsPostModalOpen] = useState(false);
+  const [isCommentsModalOpen, setIsCommentsModalOpen] = useState(false);
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+
+  const handleAssignmentsModalClick = () => {
+    setIsAssignmentsModalOpen(!isAssignmentsModalOpen);
+  };
+  const handlePostModalClick = () => {
+    setIsPostModalOpen(!isPostModalOpen);
+  };
+  const handleCommentsModalClick = () => {
+    setIsCommentsModalOpen(!isCommentsModalOpen);
+  };
+
+  const handleDetailModalClick = () => {
+    setIsDetailModalOpen(!isDetailModalOpen);
+  };
+
   const userId = useAppSelector(state => state.userInfo.id);
+  const {
+    data: assignmentData,
+    isLoading: assignmentLoading,
+    isError: assignmentError,
+    error: assignmentFetchError,
+  } = useGetAssignments(userId);
+  console.log("assignmentData::: ", assignmentData);
+
   const {
     data: myPostData,
     isLoading: myPostLoading,
     isError: myPostError,
     error: myPostFetchError,
   } = useGetMyPosts(userId);
+  console.log("useGetMyPosts::myPostData::", myPostData);
 
   const {
     data: lectureCommentData,
@@ -26,13 +54,20 @@ export default function UserActivityList() {
     error: lectureCommentFetchError,
   } = useGetLectureComments(userId);
 
-  const {
-    data: assignmentData,
-    isLoading: assignmentLoading,
-    isError: assignmentError,
-    error: assignmentFetchError,
-  } = useGetAssignments(userId);
-  console.log(assignmentData);
+  console.log("lectureCommentData::: ", lectureCommentData);
+
+  // 내가 제출한 과제
+  const filteredAssignments = assignmentData?.map(assignment => ({
+    id: assignment.id,
+    title: assignment.AssignmentData?.title,
+    content: assignment.attachmentFiles.url
+      ? `첨부파일${assignment.attachmentFiles.length}`
+      : assignment.links,
+    category: assignment.AssignmentData?.level,
+    createdAt: assignment.createdAt,
+  }));
+
+  // console.log("filteredAssignments ::", filteredAssignments);
 
   // 내가 쓴 글
   const filteredPosts = myPostData
@@ -44,6 +79,7 @@ export default function UserActivityList() {
       category: post.category,
       createdAt: post.createdAt,
     }));
+  // console.log("filteredPosts", filteredPosts);
 
   // 내가 쓴 댓글
   const filteredComments = myPostData
@@ -55,8 +91,9 @@ export default function UserActivityList() {
       category: comment.parentData.category,
       createdAt: comment.createdAt,
     }));
-  // 내가 쓴 강의 댓글
+  // console.log("filteredComments", filteredComments);
 
+  // 내가 쓴 강의 댓글
   const filteredLectureComments = lectureCommentData?.map(comment => ({
     id: comment.id,
     title: comment.parentData.title,
@@ -65,15 +102,7 @@ export default function UserActivityList() {
     createdAt: comment.createdAt,
   }));
 
-  // 내가 제출한 과제
-
-  const filteredAssignments = assignmentData?.map(assignment => ({
-    id: assignment.id,
-    title: assignment.AssignmentData.title,
-    content: assignment.content,
-    category: assignment.AssignmentData.level,
-    createdAt: assignment.createdAt,
-  }));
+  // console.log("filteredLectureComments", filteredLectureComments);
 
   const comments = [
     ...(filteredComments || []),
@@ -86,10 +115,74 @@ export default function UserActivityList() {
 
   return (
     <>
-      <div className="grid">
-        <Category title="제출한 과제" targetData={filteredAssignments} />
-        <Category title="나의 게시글" targetData={filteredPosts} />
-        <Category title="나의 댓글" targetData={comments} />
+      <div className="flex gap-4 mt-10">
+        <Category
+          title="제출한 과제"
+          targetData={filteredAssignments}
+          handleClick={handleAssignmentsModalClick}
+        />
+        {isAssignmentsModalOpen && (
+          <ModalWrapper
+            onCloseModal={() =>
+              setIsAssignmentsModalOpen(!isAssignmentsModalOpen)
+            }
+            width="748px"
+            modalTitle="제출한 과제"
+            children={
+              <Category
+                title=""
+                targetData={filteredAssignments}
+                handleClick={handleAssignmentsModalClick}
+              />
+            }
+          />
+        )}
+        <Category
+          title="나의 게시글"
+          targetData={filteredPosts}
+          handleClick={handlePostModalClick}
+        />
+        {isPostModalOpen && (
+          <ModalWrapper
+            onCloseModal={() => setIsPostModalOpen(!isPostModalOpen)}
+            width="748px"
+            modalTitle="나의 게시글"
+            children={
+              <Category
+                title=""
+                targetData={filteredPosts}
+                handleClick={handlePostModalClick}
+              />
+            }
+          />
+        )}
+        <Category
+          title="나의 댓글"
+          targetData={comments}
+          handleClick={handleCommentsModalClick}
+        />
+        {isCommentsModalOpen && (
+          <ModalWrapper
+            onCloseModal={() => setIsCommentsModalOpen(!isCommentsModalOpen)}
+            width="748px"
+            modalTitle="나의 댓글"
+            children={
+              <Category
+                title=""
+                targetData={comments}
+                handleClick={handleCommentsModalClick}
+              />
+            }
+          />
+        )}
+
+        {isDetailModalOpen && (
+          <ModalWrapper
+            onCloseModal={() => setIsDetailModalOpen(!isDetailModalOpen)}
+            width="748px"
+            children={<CommunityModal />}
+          />
+        )}
       </div>
     </>
   );
