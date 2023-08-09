d28f249bd4998c6cdaed24805bdbb8de76fbb252
lgrin-byte <lgrin02@naver.com>
2023-08-10 00:11:43 +0900

    Fix: 마이페이지 서버사이드렌더링으로 변경

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-
 import Reminder from "@/components/Mypage/Reminder";
 import Sidebar from "./(components)/Button";
 import Progress from "./(components)/Progress";
 import Profile from "./(components)/Profile";
 import UserActivityList from "./(components)/UserActivityList";
-import { useAppSelector } from "@/redux/store";
-import useGetUserQuery from "@/hooks/reactQuery/navbar/useGetUserQuery";
 
 export default function MyPage() {
-  const userId = useAppSelector(state => state.userInfo.id);
-
-  const {
-    data: userData,
-    isLoading: userLoading,
-    isError: userError,
-    error: userFetchError,
-  } = useGetUserQuery(userId);
-
   return (
     <div className="flex justify-center items-center ">
       <div className="w-9/12 flex mb-[100px] justify-center ">
