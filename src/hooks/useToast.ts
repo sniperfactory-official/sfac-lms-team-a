a679c25e63de52f9ef609f9e60a1265e221792dc
Jin <jinnkimm7@gmail.com>
2023-08-10 02:36:12 +0900

    Update: 토스트 사라지는 조건 변경, Props 타입 import

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,11 +1,5 @@
+import { ToastProps } from "sfac-designkit-react/dist/Toast";
 import { useState, useEffect } from "react";
-
-export interface ToastProps {
-  type: "Simple" | "Success" | "Error" | "With Action" | "With Title";
-  // button?: boolean;
-  // title?: string;
-  text: string;
-}
 
 export const useToast = () => {
   const [toastProps, setToastProps] = useState<ToastProps | null>(null);
@@ -13,14 +7,21 @@ export const useToast = () => {
 
   useEffect(() => {
     if (toastProps) {
-      // 클릭 이벤트가 발생하면 토스트를 닫는다.
+      // 3초 후에 토스트 닫기
+      const timer = setTimeout(() => {
+        handleCloseToast();
+      }, 3000);
+
+      // 클릭 이벤트가 발생하면 토스트를 닫기
       const handleWindowClick = () => {
+        clearTimeout(timer);
         handleCloseToast();
       };
 
       window.addEventListener("click", handleWindowClick);
 
       return () => {
+        clearTimeout(timer);
         window.removeEventListener("click", handleWindowClick);
       };
     }
